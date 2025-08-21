e50d6b58e728e2281de46acfa547fe97f05065aa
reintroll <96334440+reintroll@users.noreply.github.com>
2025-08-21 18:31:23 +0200

    Update Hola.py

--- Hola.py
+++ Hola.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 st.set_page_config(
     page_title="Demo Clasificador de Leads",
     page_icon="🎯",
