02a1dc15c9d798844ca205520375bdbaae33c3eb
reintroll <96334440+reintroll@users.noreply.github.com>
2025-08-21 18:37:38 +0200

    Update Hola.py

--- Hola.py
+++ Hola.py
@@ -1,18 +1,11 @@
 import streamlit as st
 
 #hide github icon
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 st.set_page_config(
     page_title="Demo Clasificador de Leads",
